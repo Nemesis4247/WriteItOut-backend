83ad39a14afff1cf23d1623771f10bf5d379efdc
HemantYadav <hy27.1999@gmail.com>
2020-10-27 22:55:47 +0530

    modified add_question api

--- controllers/Create/add_question.js
+++ controllers/Create/add_question.js
@@ -35,18 +35,37 @@ const handleAddQuestion = (db) => (req, res) => {
   })
   .then(data => {
     if(data.length>0){
-      res.status(200)
-             .json(
-               {
-                  'status': true,
-                  'data': {
-                    'message': data[0]
-                  }
-               }
-             );
+
+      db.select('queid', 'que', 'upvotes', 'tags', 'datetime', 'questions.userid', 'name', 'description', 'imagepath')
+      .from('questions')
+      .innerJoin('users', 'users.userid', 'questions.userid')
+      .orderBy('datetime', 'desc')
+      .then(data => {
+          return res.status(200)
+                 .json(
+                   {
+                      'status': true,
+                      'data': {
+                        'message': data
+                      }
+                   }
+                 );
+      })
+      .catch(err => {
+        return res.status(200)
+               .json(
+                 {
+                    'status': false,
+                    'data': {
+                      'message': err
+                    }
+                 }
+               );
+      });
+
     }
     else{
-      res.status(200)
+      return res.status(200)
              .json(
                {
                   'status': false,
